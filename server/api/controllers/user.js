5b4420a3458fdf81c44adc81986d4496b14d8f41
unknown <amirmohammadfiroozmehr77@gmail.com>
2019-09-05 22:11:00 +0430

    change user model and adding routes

--- server/api/controllers/user.js
+++ server/api/controllers/user.js
@@ -1,137 +1,229 @@
 const mongoose = require("mongoose");
 const User = require("../models/user");
-const config = require('config')
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const jwtSecret = config.get('app.webServer.jwtSecret');
-const { validationResult } = require('express-validator');
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const jwtSecret = config.get("app.webServer.jwtSecret");
+const { validationResult } = require("express-validator");
 const handler = (json, res, code) => {
-    res.status(code).json(json);
+  res.status(code).json(json);
+};
+const mong = id => {
+  return mongoose.Types.ObjectId(id);
+};
+const editItems = (req, text = "") => {
+  json = {};
+  if (req.body.score != undefined) json[`${text}score`] = req.body.score;
+  if (req.body.date != undefined) json[`${text}date`] = req.body.date;
+  if (req.body.passed != undefined) json[`${text}passed`] = req.body.passed;
+  if (req.body.tryCount != undefined)
+    json[`${text}tryCount`] = req.body.tryCount;
+  if (req.body.anotherChanceDate != undefined)
+    json[`${text}anotherChanceDate`] = req.body.anotherChanceDate;
+  return json;
 };
 
 module.exports.addAUser = (req, res) => {
-    const user = new User({
-        _id: mongoose.Types.ObjectId(),
-        name: req.body.name
+  const user = new User({
+    _id: mongoose.Types.ObjectId(),
+    name: req.body.name
+  })
+    .save()
+    .then(result => {
+      res.status(200).json(result);
     })
-        .save()
-        .then(result => {
-            res.status(200).json(result);
-        })
-        .catch(err => {
-            handler(err, res, 500);
-        });
-}
+    .catch(err => {
+      handler(err, res, 500);
+    });
+};
 
 module.exports.showAllUsers = (req, res) => {
-    User.find({}).exec()
-        .then(result => {
-            handler(result, res, 200)
-        })
-        .catch(err => {
-            handler(err, res, 404)
-        })
-}
+  User.find({})
+    .exec()
+    .then(result => {
+      handler(result, res, 200);
+    })
+    .catch(err => {
+      handler(err, res, 404);
+    });
+};
 
 module.exports.signup = (req, res) => {
-    if (validationResult(req).errors.length > 0) {
-        return res.status(422).json(validationResult(req));
-    }
-    User.find({
-        email: req.body.email
-    }).then((user) => {
-        if (user.length > 0) {
-            return res.status(401).json({
-                message: "email already exist"
+  if (validationResult(req).errors.length > 0) {
+    return res.status(422).json(validationResult(req));
+  }
+  User.find({
+    email: req.body.email
+  }).then(user => {
+    if (user.length > 0) {
+      return res.status(401).json({
+        message: "email already exist"
+      });
+    } else {
+      bcrypt
+        .hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              err
+            });
+          } else {
+            new User({
+              _id: mongoose.Types.ObjectId(),
+              email: req.body.email,
+              username: req.body.username,
+              password: hash,
+              role: "normal"
             })
-        }
-        else {
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                if (err) {
-                    return res.status(500).json({
-                        err
-                    })
-                }
-                else {
-                    new User({
-                        _id: mongoose.Types.ObjectId(),
-                        email: req.body.email,
-                        username: req.body.username,
-                        password: hash,
-                        role: "normal"
-                    }).save().then(user => {
-                        return res.status(200).json({
-                            message: "sign up compelete"
-                        })
-                    }).catch(err => {
-                        return res.status(500).json({
-                            err
-                        })
-                    })
-                }
-            }).catch(err => {
+              .save()
+              .then(user => {
+                return res.status(200).json({
+                  message: "sign up compelete"
+                });
+              })
+              .catch(err => {
                 return res.status(500).json({
-                    err
-                })
-            })
-
-
-
-        }
-    })
-
-
-}
-
+                  err
+                });
+              });
+          }
+        })
+        .catch(err => {
+          return res.status(500).json({
+            err
+          });
+        });
+    }
+  });
+};
 
 module.exports.signin = (req, res) => {
-
-    User.findOne({
-        email: req.body.email
-    }).exec().then((user) => {
-        if (!user) {
-            return res.status(401).json({
-                message: "email not exist"
-            })
-        }
-        else {
-            console.log(user)
-            bcrypt.compare(req.body.password, user.password, (err, same) => {
+  User.findOne({
+    email: req.body.email
+  })
+    .exec()
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "email not exist"
+        });
+      } else {
+        console.log(user);
+        bcrypt.compare(req.body.password, user.password, (err, same) => {
+          if (err) {
+            return res.status(500).json({
+              message: "cannot compare"
+            });
+          } else if (same) {
+            const jwtpayload = {
+              name: user.name,
+              email: user.email,
+              userId: user._id,
+              userName: user.username,
+              role: user.role
+            };
+            jwt.sign(
+              jwtpayload,
+              jwtSecret,
+              {
+                expiresIn: "10h"
+              },
+              (err, encoded) => {
                 if (err) {
-                    return res.status(500).json({
-                        message: "cannot compare"
-                    })
+                  throw new Error("error in jwt");
+                } else {
+                  return res.status(200).json({
+                    done: true,
+                    secret: "bearer " + encoded
+                  });
                 }
-                else if (same) {
-
-                    const jwtpayload = {
-                        name: user.name,
-                        email: user.email,
-                        userId: user._id,
-                        userName: user.username,
-                        role: user.role
-                    };
-                    jwt.sign(
-                        jwtpayload,
-                        jwtSecret, {
-                            expiresIn: '10h'
-                        },
-                        (err, encoded) => {
-                            if (err) {
-                                throw new Error('error in jwt');
-                            } else {
-                                return res.status(200).json({
-                                    done: true,
-                                    secret: 'bearer ' + encoded
-                                });
-                            }
-                        }
-                    );
-
-
-                }
-            })
+              }
+            );
+          }
+        });
+      }
+    });
+};
+module.exports.lessonRegister = (req, res) => {
+  find = {
+    _id: req.body.userId,
+    "reg_lessons.lessonId": { $ne: mong(req.body.lessonId) }
+  };
+  User.findOneAndUpdate(
+    find,
+    {
+      $push: {
+        reg_lessons: {
+          lessonId: req.body.lessonId
         }
+      }
+    },
+    { new: true }
+  )
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
     })
-
-}
+    .catch(err => {
+      handler(err, res, 500);
+    });
+};
+module.exports.sessionRegister = (req, res) => {
+  find = {
+    _id: req.body.userId,
+    "reg_lessons._id": req.body.reg_lessonId
+  };
+  User.updateOne(
+    find,
+    {
+      $push: {
+        "reg_lessons.$.reg_sessions": {
+          sessionId: req.body.sessionId,
+          title: req.body.title
+        }
+      }
+    },
+    { new: true }
+  )
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      handler(err, res, 500);
+    });
+};
+module.exports.sessionComplete = (req, res) => {
+  newItems = editItems(req, "reg_lessons.$[].reg_sessions.$[elem].");
+  find = {
+    _id: req.body.userId,
+    "reg_lessons._id": req.body.reg_lessonId,
+    "reg_lessons.reg_sessions._id": req.body.reg_sessionId
+  };
+  User.updateOne(
+    find,
+    {
+      $set: newItems
+    },
+    {
+      new: true,
+      arrayFilters: [{ "elem._id": mong(req.body.reg_sessionId) }]
+    }
+  )
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      handler(err, res, 500);
+    });
+};
+module.exports.deleteAllUsers = (req, res) => {
+  User.deleteMany({})
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      handler(err, res, 500);
+    });
+};
--- server/api/models/user.js
+++ server/api/models/user.js
@@ -1,19 +1,36 @@
-const mongoose=require('mongoose')
-const userSchema=mongoose.Schema({
-    _id:{type:mongoose.Schema.Types.ObjectId,require:true},
-    username:{type:String,default: null},
-    password:{type:String,default: null},
-    email:{type:String,default: null},
-    name:{type:String,require:true},
-    phoneNumber:{type:String,default: null},
-    gender:{type:String,enum:["man","woman"],default: "man"},
-    role:{type:String,default:"teacher"},
-    confirmed:{type:Boolean,default:true},
-    nationalCode:{type:String,default:null},
-    date_jalali:{type:Object , default: null},
-    date:{type:mongoose.Schema.Types.Date , default: Date.now},
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, require: true },
+  username: { type: String, default: null },
+  password: { type: String, default: null },
+  email: { type: String, default: null },
+  name: { type: String, require: true },
+  phoneNumber: { type: String, default: null },
+  gender: { type: String, enum: ["man", "woman"], default: "man" },
+  role: { type: String, default: "teacher" },
+  confirmed: { type: Boolean, default: true },
+  nationalCode: { type: String, default: null },
+  date_jalali: { type: Object, default: null },
+  date: { type: mongoose.Schema.Types.Date, default: Date.now },
+  reg_lessons: [
+    {
+      title: { type: String, default: null },
+      lessonId: { type: mongoose.Schema.Types.ObjectId, require: true },
+      reg_sessions: [
+        {
+          title: { type: String, require: true },
+          sessionId: { type: mongoose.Schema.Types.ObjectId, require: true },
+          score:{ type: Number, default: null },
+          tryCount:{ type: Number, default: 0 },
+          anotherChanceDate:{ type: mongoose.Schema.Types.Date, default: null },
+          date:{ type: mongoose.Schema.Types.Date, default: null },
+          passed:{type:Boolean,default:false}
 
-})
+        }
+      ]
+    }
+  ]
+});
 
-module.exports=mongoose.model('User',userSchema)
+module.exports = mongoose.model("User", userSchema);
 //default null for testing
