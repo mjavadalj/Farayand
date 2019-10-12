36b46f9e76dc4ecb48bbae9f7c63434e6d1f5b0e
amirfiroozz <amirmohammadfiroozmehr77@gmail.com>
2019-10-12 11:49:01 +0330

    role routing

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,19 +4,34 @@
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   created() {
-    const currentPath = this.$router.history.current.path;
+    const decoded = JSON.parse(this.$cookie.get("authorization"));
+    console.log(decoded);
 
-    if (window.localStorage.getItem('authenticated') === 'false') {
-      this.$router.push('/login');
+    if (!decoded) {
+      return this.$router.push("/login");
+    }
+    switch (decoded.role) {
+      case "student":
+        this.$router.push("/");
+        break;
+      case "teacher":
+        this.$router.push("/teacher");
+        break;
+      case "admin":
+        this.$router.push("/app");
+        break;
+    }
+    const currentPath = this.$router.history.current.path;
+    if (window.localStorage.getItem("authenticated") === "false") {
+      this.$router.push("/login");
     }
 
     // if (currentPath === '/' || currentPath === '/app') {
     //   this.$router.push('/app/dashboard');
     // }
-    
-  },
+  }
 };
 </script>
 
